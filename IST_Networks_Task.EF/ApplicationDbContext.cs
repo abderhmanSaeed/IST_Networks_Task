f95f60773f57f631556acd295cabf52f81225f58
Abd Elrhman Saeed <abderhmansaeed2020@gmail.com>
2024-12-31 09:33:54 +0200

    initialize .NET Core 8 application with a three-layer architecture
    - Created solution file for IST_Networks_Task.
    - Added IST_Networks_Task.Api project for API layer.
    - Added IST_Networks_Task.Core project for business logic and core entities.
    - Added IST_Networks_Task.EF project for Entity Framework and data access.

---
+++ IST_Networks_Task.EF/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IST_Networks_Task.EF
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
