b045c03a4d8e00917b91ac76d1156ccd553af62a
Abd Elrhman Saeed <abderhmansaeed2020@gmail.com>
2024-12-31 10:19:53 +0200

    add ApplicationDbContext with the connection string to Program.cs

--- IST_Networks_Task/Program.cs
+++ IST_Networks_Task/Program.cs
@@ -1,8 +1,19 @@
+using IST_Networks_Task.EF;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Configure Entity Framework and add DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
